07802dc361e09345c956780a862e47fc525d9c14
Sumit Chavan <111274627+sumitchavan8070@users.noreply.github.com>
2024-04-04 12:19:29 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,8 +3,18 @@ const {
   getPoll,
   isPostApproved,
 } = require("../controllers/adminPollControllter");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/userController");
 
 const router = express.Router();
+
+//Register
+router.post("/register", registerController);
+
+// LOGIN || POST
+router.post("/login", loginController);
 
 router.get("/polls", getPoll);
 router.get("/polls/:postId", isPostApproved);
