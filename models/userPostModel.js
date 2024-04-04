4bac42c665c4a35a700254c9466e374ffeeb521c
Sumit Chavan <111274627+sumitchavan8070@users.noreply.github.com>
2024-04-04 10:49:25 +0530

    added user post model

---
+++ models/userPostModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+// Define the schema for user posts
+const userPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["text", "poll"], // Specify the types of posts, including 'poll'
+    default: "text", // Default type is 'text'
+  },
+  poll: {
+    question: {
+      type: String,
+      required: function () {
+        return this.type === "poll"; // Make question required only for 'poll' type posts
+      },
+    },
+    options: {
+      type: [String], // Array of strings for options
+      required: function () {
+        return this.type === "poll"; // Make options required only for 'poll' type posts
+      },
+    },
+    votes: {
+      type: Map,
+      of: Number,
+      default: {}, // Map to store votes for each option
+    },
+  },
+  // Other fields as needed
+});
+
+// Create the UserPost model based on the schema
+const UserPost = mongoose.model("UserPost", userPostSchema);
+
+module.exports = UserPost;
