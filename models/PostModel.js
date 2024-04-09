be2f6eaf1f439ebb041b8ad90188428ec0744843
Sumit Chavan <111274627+sumitchavan8070@users.noreply.github.com>
2024-04-09 09:42:12 +0530

    Add files via upload
    Post Model Added

---
+++ models/PostModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isSponsored: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    approved: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
