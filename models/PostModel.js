ac266405e88cc9d5d22588cced30854119e6f76b
Sumit Chavan <111274627+sumitchavan8070@users.noreply.github.com>
2024-04-10 11:24:37 +0530

    Update PostModel.js

--- models/PostModel.js
+++ models/PostModel.js
@@ -10,7 +10,7 @@ const postSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    userId: {
+    postedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
