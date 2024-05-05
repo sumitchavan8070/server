e71cfdd8e78516cc864bc924bfdd6aeb3a8c2356
Sumit Chavan <sdchavan8070@gmail.com>
2024-05-05 16:40:36 +0530

    Delete models directory

--- models/newExamCategory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const examCategorySchema = new mongoose.Schema({
-  catName: { type: String, required: true, unique: true },
-  description: { type: String },
-  image: { type: String },
-});
-
-module.exports = mongoose.model("ExamCategory", examCategorySchema);
